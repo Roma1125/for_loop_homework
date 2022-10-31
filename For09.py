fce6c679bac4b493b011f063fe2f7a86eec65d68
Roma1125 <ramazonomonturdiyev17@gmail.com>
2022-10-31 13:16:08 +0500

    For09.py

--- For09.py
+++ For09.py
@@ -6,4 +6,10 @@ def main(price):
     Returns:
         list: return  answer
     """
-    return
+    d=[]
+    for i in range(1,11):
+        d.append(i*price)
+
+
+    return d
+print(main(2.25))
