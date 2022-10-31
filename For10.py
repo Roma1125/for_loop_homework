bf6f20bb2834366c220dc5881421b7cefb0248e9
Roma1125 <ramazonomonturdiyev17@gmail.com>
2022-10-31 13:37:49 +0500

    For10.py

--- For10.py
+++ For10.py
@@ -6,4 +6,9 @@ def main(list1):
     Returns:
         list: return  answer
     """
-    return
+    d=[]
+    for i in list1:
+        d.append(i.title())
+        
+    return d
+print(main(['rustam', 'diyor', 'alisher', 'bektosh']))
